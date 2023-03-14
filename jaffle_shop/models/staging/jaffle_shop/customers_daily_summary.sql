b0318bf0ff18f29bf6a123a1089116517bec00c2
Jarrod O'Callaghan <12422677+JarrodOCallaghan@users.noreply.github.com>
2023-03-14 16:16:36 +1100

    Updated code for jinja macro walkthrough

--- jaffle_shop/models/staging/jaffle_shop/customers_daily_summary.sql
+++ jaffle_shop/models/staging/jaffle_shop/customers_daily_summary.sql
@@ -1,8 +1,8 @@
 select 
 
-    {{ dbt_utils.surrogate_key(['customer_id', 'order_date']) }} as id,
+    {{ dbt_utils.generate_surrogate_key(['customer_id', 'order_date']) }} as id,
     customer_id,
     order_date,
-    count(*)
+    count(*) as count
 from {{ ref('stg_orders') }}
 group by 1,2,3
